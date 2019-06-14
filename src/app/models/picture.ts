b901a25f50fc80b657b1f10facfca1eaaab0cf6a
Markus Weser <git@MarkusWeser.info>
2019-06-14 10:40:09 +0100

    feat: prepare Picture interface

---
+++ src/app/models/picture.ts
@@ -0,0 +1,15 @@
+export interface Comment {
+  id?: number;
+  fromName?: string;
+  comment?: string;
+}
+
+export interface Picture {
+  id?: number;
+  title?: string;
+  imageUrl?: string;
+  description?: string;
+  rate: number; // one to five stars?
+  authorName?: string;
+  comments?: Comment[];
+}
